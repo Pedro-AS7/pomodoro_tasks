c97a40e0048ec33cdc1321b20c25e3ec35965484
Pedro Simões <pedrodev.fps@gmail.com>
2025-08-21 19:00:24 -0300

    Favicon

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,8 +3,9 @@ import TimerContainerGem from './components/timer_components/Timer'
 
 function App() {
 	return (
-		<main className="bg-[#ffffff] w-full h-screen flex flex-col items-center gap-6 lg:gap-10 border-t-4 border-orange-500 p-4 sm:p-6 lg:p-8">
+		<main className="bg-[#ffffff] w-full h-screen flex flex-col items-center gap-6 lg:gap-10 border-t-4 border-orange-500 p-4 sm:p-6 lg:p-6">
 			<nav className="w-full flex items-center">
+                <img src="../public/favicon.png" alt="" width={50}/>
 				<h1 className="text-gray-700 text-xl sm:text-2xl font-bold">Pomodoro Tasks</h1>
 			</nav>
 
